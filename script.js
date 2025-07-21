1f5915399e13eb58db5f7c08144cb2e8841eb3c0
rahulnikamcyber <rahulnikamcyber@gmail.com>
2025-07-21 19:07:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ document.getElementById('menu-toggle').addEventListener('click', function () {
   const navLinks = document.getElementById('nav-links');
   navLinks.classList.toggle('show');
 });
+.counters {
+  background: #fff;
+  padding: 60px 20px;
+  text-align: center;
+}
+
+.counter-box {
+  display: inline-block;
+  margin: 20px;
+  font-size: 2.5rem;
+  color: #002244;
+}
+
+.counter-box p {
+  font-size: 1rem;
+  margin-top: 10px;
+  color: #444;
+}
+
